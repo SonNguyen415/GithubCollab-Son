84388a72cd7a961b758c5adc80e75339f86b6cf9
Son Nguyen <snguyen21@ssis.edu.vn>
2020-03-20 06:29:52 +0700

    Revert "Commit"

--- novel.py
+++ novel.py
@@ -34,8 +34,7 @@ def insert_novels(nName, nGenre, nID, aID):
     """
     insertion = ('INSERT INTO Novels (Name, Genre, NovelID, AuthorID) Values ("' + str(nName) + '", "' + str(nGenre) + '", ' + str(nID) + ', ' + str(aID) + ')');
     sql = c.execute(insertion)
-    # I wanna save
-    con.commit()
+    # I don't wanna save
     
 def render_menu():
     """
